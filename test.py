ae2ff3115cfec61f3838a98dfe4d2e03400db06d
abdulrahman <abdulrahman22aama@gmail.com>
2025-04-09 17:00:12 +0300

    the test is Done!

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ print("Just for testing the git if he is commit the modified or not .")
 
 
 for i in range(1,11):
-    print(i)
+    print('Abdulrahman Ahmed Muhammed Alhelali.')
+
+
+
+
+print('Hello Git how you to fealing today!')
 
